90a39ab14b1a9f84f89a49efe6b9b62062e78601
Tobias Maile <tobias@maileconsulting.de>
2019-04-30 17:50:23 +0200

    more cleanup

--- lib/buildingsync/translator.rb
+++ lib/buildingsync/translator.rb
@@ -28,8 +28,11 @@ module BuildingSync
       @doc.elements.each("#{@ns}:BuildingSync/#{@ns}:Facilities/#{@ns}:Facility/") { |facility| facilities << facility }
       raise 'BuildingSync file must have exactly 1 facility' if facilities.size != 1
 
-      # choose the correct workflow maker based on xml
+      # choose the correct model maker based on xml
       choose_model_maker
+
+      # choose the correct workflow maker based on xml
+      choose_workflow_maker
     end
 
     private
@@ -37,6 +40,11 @@ module BuildingSync
     def choose_model_maker
       # for now there is only one workflow maker
       @model_maker = ModelMakerLevelZero.new(@doc, @ns)
+    end
+
+    def chooseWorkflowMaker
+      # for now there is only one workflow maker
+      @workflow_maker = PhaseZeroWorkflowMaker.new(@doc, @ns)
     end
   end
 end
