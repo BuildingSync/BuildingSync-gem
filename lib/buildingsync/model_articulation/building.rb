7067942307752b115798243918adfe1924c6f7b8
TMaile <tobias.maile@d-alchemy.com>
2019-05-03 16:54:36 +0200

    adding the calibration function

--- lib/buildingsync/model_articulation/building.rb
+++ lib/buildingsync/model_articulation/building.rb
@@ -10,7 +10,6 @@ module BuildingSync
     @num_stories_above_grade = nil
     @num_stories_below_grade = nil
     @ns_to_ew_ratio = nil
-
     @building_rotation = 0.0 # setDefaultValue
     @floor_height = 0.0 # ft -> m -- setDefaultValue in ft
     @wwr = 0.0 # setDefaultValue in fraction
@@ -27,6 +26,12 @@ module BuildingSync
       @wwr = 0.0
       @name = nil
       @model = nil
+      # variables not used during read xml for now
+      @party_wall_stories_north = 0
+      @party_wall_stories_south = 0
+      @party_wall_stories_west = 0
+      @party_wall_stories_east = 0
+      @party_wall_fraction = 0
       # code to initialize
       read_xml(build_element, ns)
     end
@@ -233,7 +238,9 @@ module BuildingSync
       # remove non-resource objects not removed by removing the building
       remove_non_resource_objects(runner, @model)
 
-      party_walls_array = {}
+      # party_walls_array to be used by orientation specific or fractional party wall values
+      party_walls_array = generate_party_walls # this is an array of arrays, where each entry is effective building story with array of directions
+
       # populate bar hash with story information
       bar_hash[:stories] = {}
       num_stories.ceil.times do |i|
@@ -295,6 +302,338 @@ module BuildingSync
       puts "INFO: The building finished with #{@model.getSpaces.size} spaces."
 
       return true
+    end
+
+    def generate_party_walls
+      party_walls_array = []
+      if @party_wall_stories_north + @party_wall_stories_south + @party_wall_stories_east + @party_wall_stories_west > 0
+
+        # loop through effective number of stories add orientation specific party walls per user arguments
+        num_stories.ceil.times do |i|
+          test_value = i + 1 - bar_hash[:num_stories_below_grade]
+
+          array = []
+          if @party_wall_stories_north >= test_value
+            array << 'north'
+          end
+          if @party_wall_stories_south >= test_value
+            array << 'south'
+          end
+          if @party_wall_stories_east >= test_value
+            array << 'east'
+          end
+          if @party_wall_stories_west >= test_value
+            array << 'west'
+          end
+
+          # populate party_wall_array for this story
+          party_walls_array << array
+        end
+      end
+
+      # calculate party walls if using party_wall_fraction method
+      if @party_wall_fraction > 0 && !party_walls_array.empty?
+        runner.registerWarning('Both orientaiton and fractional party wall values arguments were populated, will ignore fractional party wall input')
+      elsif @party_wall_fraction > 0
+
+        # orientation of long and short side of building will vary based on building rotation
+
+        # full story ext wall area
+        typical_length_facade_area = length * floor_height
+        typical_width_facade_area = width * floor_height
+
+        # top story ext wall area, may be partial story
+        partial_story_multiplier = (1.0 - @num_stories_above_grade.ceil + @num_stories_above_grade)
+        area_multiplier = partial_story_multiplier
+        edge_multiplier = Math.sqrt(area_multiplier)
+        top_story_length = length * edge_multiplier
+        top_story_width = width * edge_multiplier
+        top_story_length_facade_area = top_story_length * floor_height
+        top_story_width_facade_area = top_story_width * floor_height
+
+        total_exterior_wall_area = 2 * (length + width) * (@num_stories_above_grade.ceil - 1.0) * floor_height + 2 * (top_story_length + top_story_width) * floor_height
+        target_party_wall_area = total_exterior_wall_area * @party_wall_fraction
+
+        width_counter = 0
+        width_area = 0.0
+        facade_area = typical_width_facade_area
+        until (width_area + facade_area >= target_party_wall_area) || (width_counter == @num_stories_above_grade.ceil * 2)
+          # update facade area for top story
+          if width_counter == @num_stories_above_grade.ceil - 1 || width_counter == @num_stories_above_grade.ceil * 2 - 1
+            facade_area = top_story_width_facade_area
+          else
+            facade_area = typical_width_facade_area
+          end
+
+          width_counter += 1
+          width_area += facade_area
+
+        end
+        width_area_remainder = target_party_wall_area - width_area
+
+        length_counter = 0
+        length_area = 0.0
+        facade_area = typical_length_facade_area
+        until (length_area + facade_area >= target_party_wall_area) || (length_counter == @num_stories_above_grade.ceil * 2)
+          # update facade area for top story
+          if length_counter == @num_stories_above_grade.ceil - 1 || length_counter == @num_stories_above_grade.ceil * 2 - 1
+            facade_area = top_story_length_facade_area
+          else
+            facade_area = typical_length_facade_area
+          end
+
+          length_counter += 1
+          length_area += facade_area
+        end
+        length_area_remainder = target_party_wall_area - length_area
+
+        # get rotation and best fit to adjust orientation for fraction party wall
+        rotation = args['building_rotation'] % 360.0 # should result in value between 0 and 360
+        card_dir_array = [0.0, 90.0, 180.0, 270.0, 360.0]
+        # reverse array to properly handle 45, 135, 225, and 315
+        best_fit = card_dir_array.reverse.min_by { |x| (x.to_f - rotation).abs }
+
+        if ![90.0, 270.0].include? best_fit
+          width_card_dir = ['east', 'west']
+          length_card_dir = ['north', 'south']
+        else # if rotation is closest to 90 or 270 then reverse which orientation is used for length and width
+          width_card_dir = ['north', 'south']
+          length_card_dir = ['east', 'west']
+        end
+
+        # if dont' find enough on short sides
+        if width_area_remainder <= typical_length_facade_area
+
+          num_stories.ceil.times do |i|
+            if i + 1 <= @num_stories_below_grade
+              party_walls_array << []
+              next
+            end
+            if i + 1 - @num_stories_below_grade <= width_counter
+              if i + 1 - @num_stories_below_grade <= width_counter - @num_stories_above_grade
+                party_walls_array << width_card_dir
+              else
+                party_walls_array << [width_card_dir.first]
+              end
+            else
+              party_walls_array << []
+            end
+          end
+
+        else # use long sides instead
+
+          num_stories.ceil.times do |i|
+            if i + 1 <= @num_stories_below_grade
+              party_walls_array << []
+              next
+            end
+            if i + 1 - @num_stories_below_grade <= length_counter
+              if i + 1 - @num_stories_below_grade <= length_counter - @num_stories_above_grade
+                party_walls_array << length_card_dir
+              else
+                party_walls_array << [length_card_dir.first]
+              end
+            else
+              party_walls_array << []
+            end
+          end
+        end
+        # TODO: - currently won't go past making two opposing sets of walls party walls. Info and registerValue are after create_bar in measure.rb
+      end
+      party_walls_array
+    end
+
+    def calibrate_baseline_model(template, bldg_type, skip)
+      @@calibrate_factors = JSON.parse(File.read(File.dirname(__FILE__) + '/resources/calibrate_factors.json'))
+      pd_change_rate = @@calibrate_factors[template][bldg_type]['lpd_change_rate']
+      epd_change_rate = @@calibrate_factors[template][bldg_type]['epd_change_rate']
+      occupancy_change_rate = @@calibrate_factors[template][bldg_type]['occupancy_change_rate']
+      cop_change_rate = @@calibrate_factors[template][bldg_type]['cop_change_rate']
+      heating_efficiency_change_rate = @@calibrate_factors[template][bldg_type]['heating_efficiency_change_rate']
+
+      # report initial condition
+      building = @model.getBuilding
+      initial_lpd = building.lightingPowerPerFloorArea # W/m^2
+      initial_epd = building.electricEquipmentPowerPerFloorArea # W/m^2
+      initial_occupancy = building.peoplePerFloorArea # people/m^2
+
+      p "lpd_change_rate: #{lpd_change_rate.to_s}"
+      p "epd_change_rate: #{epd_change_rate.to_s}"
+      p "occupancy_change_rate: #{occupancy_change_rate.to_s}"
+      p "cop_change_rate: #{cop_change_rate.to_s}"
+      p "heating_efficiency_change_rate: #{heating_efficiency_change_rate.to_s}"
+      p "initial_lpd: #{initial_lpd.round(3).to_s}"
+      p "initial_epd: #{initial_epd.round(3).to_s}"
+      p "initial_occupancy: #{initial_occupancy.round(3).to_s}"
+
+      space_types = @model.getSpaceTypes
+      # loop through space types
+      space_types.each do |space_type|
+        # Update lighting power density
+        space_type.lights.each do |light|
+          light_def = light.lightsDefinition
+          unless light_def.lightingLevel.empty?
+            light_def.setLightingLevel((1 + lpd_change_rate) * light_def.lightingLevel.get)
+          end
+
+          unless light_def.wattsperSpaceFloorArea .empty?
+            light_def.setWattsperSpaceFloorArea((1 + lpd_change_rate) * light_def.wattsperSpaceFloorArea.get)
+          end
+
+          unless light_def.wattsperPerson.empty?
+            light_def.setWattsperPerson((1 + lpd_change_rate) * light_def.wattsperPerson.get)
+          end
+        end
+
+        # Update the equipment power density
+        space_type.electricEquipment.each do |equip|
+          equip_def = equip.electricEquipmentDefinition
+          unless equip_def.designLevel.empty?
+            equip_def.setDesignLevel((1 + epd_change_rate) * equip_def.designLevel.get)
+          end
+
+          unless equip_def.wattsperSpaceFloorArea .empty?
+            equip_def.setWattsperSpaceFloorArea((1 + epd_change_rate) * equip_def.wattsperSpaceFloorArea.get)
+          end
+
+          unless equip_def.wattsperPerson.empty?
+            equip_def.setWattsperPerson((1 + epd_change_rate) * equip_def.wattsperPerson.get)
+          end
+        end
+
+        # Update the occupancy density
+        space_type.people.each do |people|
+          people_def = people.peopleDefinition
+          unless people_def.numberofPeople.empty?
+            people_def.setNumberofPeople((1 + occupancy_change_rate) * people_def.numberofPeople.get)
+          end
+
+          unless people_def.peopleperSpaceFloorArea.empty?
+            people_def.setPeopleperSpaceFloorArea((1 + occupancy_change_rate) * people_def.peopleperSpaceFloorArea.get)
+          end
+
+          unless people_def.spaceFloorAreaperPerson.empty?
+            people_def.setSpaceFloorAreaperPerson(people_def.spaceFloorAreaperPerson.get/(1 + occupancy_change_rate))
+          end
+        end
+      end
+
+      # Update HVAC systems
+      air_loops = @model.getAirLoopHVACs
+      plant_loops = @model.getPlantLoops
+
+      initial_cop_value = nil
+      after_cop_value = nil
+      double_after_cop = nil
+
+      initial_eff_value = nil
+      after_eff_value = nil
+
+      # loop through air loops
+      air_loops.each do |air_loop|
+        find_cooling = false
+        find_heating = false
+
+        # find single speed dx units on loop
+        air_loop.supplyComponents.each do |supply_component|
+          hvac_component = supply_component.to_CoilCoolingDXSingleSpeed
+          unless hvac_component.empty?
+            hvac_component = hvac_component.get
+
+            # change and report high speed cop
+            initial_cop = hvac_component.ratedCOP
+            if initial_cop.empty?
+              raise "Fail to find the Rated COP for single speed dx unit '#{hvac_component.name}' on air loop '#{air_loop.name}'"
+            else
+              initial_cop_value = initial_cop.get
+              after_cop_value = initial_cop_value * (1 + cop_change_rate)
+              double_after_cop = OpenStudio::OptionalDouble.new(after_cop_value)
+              hvac_component.setRatedCOP(after_cop_value)
+              find_cooling = true
+              raise "Fail to find the cooling system for air lop '#{air_loop.name}'" unless find_cooling
+            end
+          end
+
+          hvac_component = supply_component.to_CoilCoolingDXTwoSpeed
+          unless hvac_component.empty?
+            hvac_component = hvac_component.get
+
+            # change and report high speed cop
+            initial_cop = hvac_component.ratedHighSpeedCOP
+            if initial_cop.empty?
+              raise "Fail to find the Rated High Speed COP for two speed dx unit '#{hvac_component.name}' on air loop '#{air_loop.name}'"
+            else
+              initial_cop_value = initial_cop.get
+              after_cop_value = initial_cop_value * (1 + cop_change_rate)
+              double_after_cop = OpenStudio::OptionalDouble.new(after_cop_value)
+              hvac_component.setRatedHighSpeedCOP(after_cop_value)
+            end
+
+            # change and report low speed cop
+            initial_cop = hvac_component.ratedLowSpeedCOP
+            if initial_cop.empty?
+              raise "Fail to find the Rated Low Speed COP for two speed dx unit '#{hvac_component.name}' on air loop '#{air_loop.name}'"
+            else
+              initial_cop_value = initial_cop.get
+              after_cop_value = initial_cop_value * (1 + cop_change_rate)
+              double_after_cop = OpenStudio::OptionalDouble.new(after_cop_value)
+              hvac_component.setRatedLowSpeedCOP(after_cop_value)
+            end
+
+            find_cooling = true
+            raise "Fail to find the cooling system for air lop '#{air_loop.name}'" unless find_cooling
+          end
+
+          hvac_component = supply_component.to_CoilHeatingGas
+          unless hvac_component.empty?
+            hvac_component = hvac_component.get
+            initial_eff_value = hvac_component.gasBurnerEfficiency
+            after_eff_value = initial_eff_value * (1 + heating_efficiency_change_rate)
+            # check for reasonableness
+            if after_eff_value <= 0 or after_eff_value > 0.99
+              raise "Wrong after heating efficiency found: initial (#{initial_eff_value}), change rate (#{heating_efficiency_change_rate}), after (#{after_eff_value})."
+            end
+            hvac_component.setGasBurnerEfficiency(after_eff_value)
+            find_heating = true
+            raise "Fail to find the heating system for air lop '#{air_loop.name}'" unless find_heating
+          end
+        end
+      end
+
+      # loop through plant loops
+      plant_loops.each do |plant_loop|
+        find_heating = false
+        # find boiler on plat loop
+        plant_loop.supplyComponents.each do |supply_component|
+          hvac_component = supply_component.to_BoilerHotWater
+          unless hvac_component.empty?
+            hvac_component = hvac_component.get
+            initial_eff_value = hvac_component.nominalThermalEfficiency
+            after_eff_value = initial_eff_value * (1 + heating_efficiency_change_rate)
+            # check for reasonableness
+            if after_eff_value <= 0 or after_eff_value > 0.99
+              raise "Wrong after heating efficiency found: initial (#{initial_eff_value}), change rate (#{heating_efficiency_change_rate}), after (#{after_eff_value})."
+            end
+            hvac_component.setNominalThermalEfficiency(after_eff_value)
+            find_heating = true
+            raise "Fail to find the heating system for air lop '#{air_loop.name}'" unless find_heating
+          end
+        end
+      end
+
+      p "initial_cop: #{initial_cop_value.round(3).to_s}"
+      p "after_cop: #{after_cop_value.round(3).to_s}"
+      p "initial_eff: #{initial_eff_value.round(3).to_s}"
+      p "after_eff: #{after_eff_value.round(3).to_s}"
+
+      # report final condition
+      after_lpd = building.lightingPowerPerFloorArea
+      after_epd = building.electricEquipmentPowerPerFloorArea
+      after_occupancy = building.peoplePerFloorArea # people/m^2
+
+      p "after_lpd: #{after_lpd.round(3).to_s}"
+      p "after_epd: #{after_epd.round(3).to_s}"
+      p "after_occupancy: #{after_occupancy.round(3).to_s}"
     end
 
     def write_osm(dir)
