a65bed5f9c8f8360f315d6c047f18ab1ae2fb051
TMaile <tobias.maile@d-alchemy.com>
2020-09-10 16:52:53 +0200

    cleanup of required and added dependency on openstudio-standards

--- lib/buildingsync/model_articulation/spatial_element.rb
+++ lib/buildingsync/model_articulation/spatial_element.rb
@@ -37,13 +37,11 @@
 require 'openstudio'
 require 'fileutils'
 require 'json'
-# require_relative '../tmp_os_lib_model_generation_bricr'
 require 'openstudio/extension/core/os_lib_model_generation'
 
 module BuildingSync
   # base class for objects that will configure workflows based on building sync files
   class SpatialElement
-    #include Tmp_OsLib_ModelGenerationBRICR
 
     def initialize
       @total_floor_area = nil
