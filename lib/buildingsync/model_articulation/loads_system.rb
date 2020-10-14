ad0a657dda8010b4bb04cb23cfc4dd22d92ed8a3
TMaile <tobias.maile@d-alchemy.com>
2020-10-14 16:45:21 +0200

    moved parametric_schedule_helper file to helper directory

--- lib/buildingsync/model_articulation/loads_system.rb
+++ lib/buildingsync/model_articulation/loads_system.rb
@@ -35,7 +35,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # *******************************************************************************
 require_relative 'building_system'
-require_relative 'parametric_schedule_helper'
+require_relative '../helpers/parametric_schedule_helper'
 require 'openstudio/extension/core/os_lib_schedules.rb'
 
 module BuildingSync
