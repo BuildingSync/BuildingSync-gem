97027526eb32b3276451961a01612e47e1ad3d06
Hannah Eslinger <heslinge@nrel.gov>
2025-06-03 18:02:26 -0600

    Delete systems

--- lib/buildingsync/model_articulation/facility.rb
+++ lib/buildingsync/model_articulation/facility.rb
@@ -61,7 +61,6 @@ module BuildingSync
     def initialize(base_xml, ns)
       @base_xml = base_xml
       @ns = ns
-      @g = BuildingSync::Generator.new(ns)
 
       help_element_class_type_check(base_xml, 'Facility')
 
@@ -73,7 +72,6 @@ module BuildingSync
 
       @measures = []
       @contacts = []
-      @systems_map = {}
 
       # TODO: Go under Report
       @utility_name = nil
@@ -180,55 +178,9 @@ module BuildingSync
       return @site.determine_open_studio_system_standard
     end
 
-    # read systems
-    def read_and_create_initial_systems
-      systems_xml = xget_or_create('Systems')
-      if !systems_xml.elements.empty?
-        systems_xml.elements.each do |system_type|
-          @systems_map[system_type.name] = []
-          system_type.elements.each do |system_xml|
-            if system_xml.name == 'HVACSystem'
-              @systems_map[system_type.name] << BuildingSync::HVACSystem.new(system_xml, @ns)
-            elsif system_xml.name == 'LightingSystem'
-              @systems_map[system_type.name] << BuildingSync::LightingSystemType.new(system_xml, @ns)
-            else
-              @systems_map[system_type.name] << system_xml
-            end
-          end
-        end
-      else
-        hvac_xml = @g.add_hvac_system_to_facility(@base_xml)
-        lighting_xml = @g.add_lighting_system_to_facility(@base_xml)
-        @hvac_system = HVACSystem.new(hvac_xml, @ns)
-        @lighting_system = LightingSystemType.new(lighting_xml, @ns)
-        @load_system = LoadsSystem.new
-      end
-    end
-
     # @see BuildingSync::Report.add_cb_modeled
     def add_cb_modeled(id = 'Scenario-Baseline')
       @report.add_cb_modeled(id)
-    end
-
-    # Add a minimal lighting system in the doc and as an object
-    # @param premise_id [String] id of the premise which the system will be linked to
-    # @param premise_type [String] type of premise, i.e. Building, Section, etc.
-    # @param lighting_system_id [String] id for new lighting system
-    # @return [BuildingSync::LightingSystemType] new lighting system object
-    def add_blank_lighting_system(premise_id, premise_type, lighting_system_id = 'LightingSystem-1')
-      # Create new lighting system and link it
-      lighting_system_xml = @g.add_lighting_system_to_facility(@base_xml, lighting_system_id)
-      @g.add_linked_premise(lighting_system_xml, premise_id, premise_type)
-
-      # Create a new array if doesn't yet exist
-      if !@systems_map.key?('LightingSystems')
-        @systems_map['LightingSystems'] = []
-      end
-
-      # Create new lighting system and add to array
-      new_system = BuildingSync::LightingSystemType.new(lighting_system_xml, @ns)
-      @systems_map['LightingSystems'] << new_system
-      return new_system
     end
 
     # read other details from the xml
@@ -243,163 +195,6 @@ module BuildingSync
       end
     end
 
-    # create building systems
-    # @param main_output_dir [String] main output path, not scenario specific. i.e. SR should be a subdirectory
-    # @param zone_hash [Hash]
-    # @param hvac_delivery_type [String]
-    # @param htg_src [String]
-    # @param clg_src [String]
-    # @param add_space_type_loads [Boolean]
-    # @param add_constructions [Boolean]
-    # @param add_elevators [Boolean]
-    # @param add_exterior_lights [Boolean]
-    # @param add_exhaust [Boolean]
-    # @param add_swh [Boolean]
-    # @param add_hvac [Boolean]
-    # @param add_thermostat [Boolean]
-    # @param remove_objects [Boolean]
-    def create_building_systems(main_output_dir:, zone_hash: nil, hvac_delivery_type: 'Forced Air', htg_src: 'NaturalGas', clg_src: 'Electricity',
-                                add_space_type_loads: true, add_constructions: true, add_elevators: false, add_exterior_lights: false,
-                                add_exhaust: true, add_swh: true, add_hvac: true, add_thermostat: true, remove_objects: false)
-      model = @site.get_model
-      template = @site.get_standard_template
-      system_type = @site.get_system_type
-      climate_zone = @site.get_climate_zone
-
-      onsite_parking_fraction = 1.0
-      exterior_lighting_zone = '3 - All Other Areas'
-
-      initial_objects = model.getModelObjects.size
-
-      OpenStudio.logFree(OpenStudio::Info, 'BuildingSync.Facility.create_building_system', "The building started with #{initial_objects} objects.")
-      puts "BuildingSync.Facility.create_building_system - The building started with #{initial_objects} objects."
-
-      # TODO: systems_xml.elements["#{@ns}:LightingSystems"]
-      # Make the open_studio_system_standard applier
-      open_studio_system_standard = determine_open_studio_system_standard
-      OpenStudio.logFree(OpenStudio::Info, 'BuildingSync.Facility.create_building_system', "Building Standard with template: #{template}.")
-
-      # add internal loads to space types
-      if add_space_type_loads
-        @load_system.add_internal_loads(model, open_studio_system_standard, template, @site.get_building_sections, remove_objects)
-        new_occupancy_peak = @site.get_peak_occupancy
-        new_occupancy_peak.each do |id, occupancy_peak|
-          floor_area = @site.get_floor_area[id]
-          if occupancy_peak && floor_area && floor_area > 0.0
-            puts "new peak occupancy value found: absolute occupancy: #{occupancy_peak} occupancy per area: #{occupancy_peak.to_f / floor_area.to_f} and area: #{floor_area} m2"
-            @load_system.adjust_occupancy_peak(model, occupancy_peak, floor_area, @site.get_space_types_from_hash(id))
-          end
-        end
-      end
-
-      # identify primary building type (used for construction, and ideally HVAC as well)
-      building_types = {}
-      model.getSpaceTypes.each do |space_type|
-        # populate hash of building types
-        if space_type.standardsBuildingType.is_initialized
-          bldg_type = space_type.standardsBuildingType.get
-          if !building_types.key?(bldg_type)
-            building_types[bldg_type] = space_type.floorArea
-          else
-            building_types[bldg_type] += space_type.floorArea
-          end
-        else
-          OpenStudio.logFree(OpenStudio::Warn, 'BuildingSync.Facility.create_building_system', "Can't identify building type for #{space_type.name}")
-        end
-      end
-      primary_bldg_type = building_types.key(building_types.values.max) # TODO: - this fails if no space types, or maybe just no space types with standards
-      lookup_building_type = open_studio_system_standard.model_get_lookup_name(primary_bldg_type) # Used for some lookups in the standards gem
-      model.getBuilding.setStandardsBuildingType(primary_bldg_type)
-
-      envelope_system = nil
-      # make construction set and apply to building
-      if add_constructions
-        envelope_system = EnvelopeSystem.new
-        envelope_system.create(model, open_studio_system_standard, primary_bldg_type, lookup_building_type, remove_objects)
-      end
-
-      # add elevators (returns ElectricEquipment object)
-      if add_elevators
-        @load_system.add_elevator(model, open_studio_system_standard)
-      end
-
-      # add exterior lights (returns a hash where key is lighting type and value is exteriorLights object)
-      if add_exterior_lights
-        if !@systems_map['LightingSystems'].nil?
-          @systems_map['LightingSystems'].each do |lighting_system|
-            lighting_system.add_exterior_lights(model, open_studio_system_standard, onsite_parking_fraction, exterior_lighting_zone, remove_objects)
-          end
-        else
-          new_lighting_system = add_blank_lighting_system(@site.get_building.xget_id, 'Building')
-          new_lighting_system.add_exterior_lights(model, open_studio_system_standard, onsite_parking_fraction, exterior_lighting_zone, remove_objects)
-        end
-      end
-
-      # add_exhaust
-      if add_exhaust
-        @hvac_system.add_exhaust(model, open_studio_system_standard, 'Adjacent', remove_objects)
-      end
-
-      # add service water heating demand and supply
-      if add_swh
-        service_hot_water_system = ServiceHotWaterSystem.new
-        service_hot_water_system.add(model, open_studio_system_standard, remove_objects)
-      end
-
-      # TODO: Make this better
-      @lighting_system.add_daylighting_controls(model, open_studio_system_standard, template, main_output_dir)
-
-      # TODO: - add internal mass
-      # TODO: - add slab modeling and slab insulation
-      # TODO: - fuel customization for cooking and laundry
-      # TODO: - add refrigeration
-      # remove refrigeration equipment
-      if remove_objects
-        model.getRefrigerationSystems.each(&:remove)
-      end
-
-      # works by switching some fraction of electric loads to gas if requested (assuming base load is electric)
-      # add thermostats
-      if add_thermostat
-        @hvac_system.add_thermostats(model, open_studio_system_standard, remove_objects)
-      end
-
-      # add hvac system
-      if add_hvac
-        @hvac_system.add_hvac(model, zone_hash, open_studio_system_standard, system_type, hvac_delivery_type, htg_src, clg_src, remove_objects)
-      end
-
-      # set hvac controls and efficiencies (this should be last model articulation element)
-      if add_hvac
-        if remove_objects
-          model.purgeUnusedResourceObjects
-          objects_after_cleanup = initial_objects - model.getModelObjects.size
-          OpenStudio.logFree(OpenStudio::Warn, 'BuildingSync.Facility.create_building_system', "Removing #{objects_after_cleanup} objects from model")
-        end
-        @hvac_system.apply_sizing_and_assumptions(model, main_output_dir, open_studio_system_standard, primary_bldg_type, system_type, climate_zone)
-      end
-
-      # remove everything but spaces, zones, and stub space types (extend as needed for additional objects, may make bool arg for this)
-      if remove_objects
-        model.purgeUnusedResourceObjects
-        objects_after_cleanup = initial_objects - model.getModelObjects.size
-        if objects_after_cleanup > 0
-          OpenStudio.logFree(OpenStudio::Info, 'BuildingSync.Facility.create_building_system', "Removing #{objects_after_cleanup} objects from model")
-        end
-      end
-
-      # report final condition of model
-      OpenStudio.logFree(OpenStudio::Info, 'BuildingSync.Facility.create_building_system', "The building finished with #{model.getModelObjects.size} objects.")
-      puts "BuildingSync.Facility.create_building_system - The building finished with #{model.getModelObjects.size} objects."
-    end
-
-    # write osm
-    # @param dir [String]
-    # @return [Array]
-    def write_osm(dir)
-      scenario_types = @site.write_osm(dir)
-      return scenario_types
-    end
 
     # TODO: I don't think we want any of this.
     # write parameters to xml
@@ -407,6 +202,6 @@ module BuildingSync
       @site.prepare_final_xml
     end
 
-    attr_reader :systems_map, :site, :report, :measures, :contacts
+    attr_reader :site, :report, :measures, :contacts
   end
 end
